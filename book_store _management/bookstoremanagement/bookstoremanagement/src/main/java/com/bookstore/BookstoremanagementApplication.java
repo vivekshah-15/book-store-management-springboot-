a3dac7fe088549e6b1400f541033a9e3400727eb
Vivek Shah <132201032+vivekshah-15@users.noreply.github.com>
2024-08-02 11:35:31 +0530

    Add files via upload

---
+++ book_store _management/bookstoremanagement/bookstoremanagement/src/main/java/com/bookstore/BookstoremanagementApplication.java
@@ -0,0 +1,13 @@
+package com.bookstore;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BookstoremanagementApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BookstoremanagementApplication.class, args);
+	}
+
+}
